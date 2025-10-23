59b4cd1945870e64fac8d0b98d2d9f84843fbdf3
Alex Bol <alexbol99@gmail.com>
2025-10-23 22:46:38 +0300

    Changes from qbiq studio (#231)
    * Changes from qbiq studio
    
    Small changes raised in project developed for QbiqAI company
    
    * Box: new method extend()
    * Face: new getter vertices()
    * Polygon: new helper methods rearrange(), orientation(), isOuter(), isMultiPolygon
    * Matrix: new convertors fromMatrix3x3() and toMatrix3x3(), fixed typo in description
    * Vector: added constructor(segment: Segment),
    new method isZeroLength()
    
    * Build, generate docs, upgrade version to 1.6.6

--- src/classes/box.js
+++ src/classes/box.js
@@ -7,7 +7,7 @@ import Flatten from '../flatten';
 import {convertToString} from "../utils/attributes";
 import {Shape} from "./shape";
 import {Errors} from "../utils/errors";
-import {intersectSegment2Arc, intersectSegment2Circle} from "../algorithms/intersection";
+import {intersectSegment2Arc} from "../algorithms/intersection";
 
 /**
  * Class Box represents bounding box of the shape.
@@ -189,6 +189,21 @@ export class Box extends Shape {
         this.ymin = ymin;
         this.xmax = xmax;
         this.ymax = ymax;
+    }
+
+    /**
+     * Return new extended box
+     * @param {number} extension - positive number, how much to extend the box
+     * @returns {Box}
+     */
+    extend(extension) {
+        if (extension <= 0) return this.clone();
+        return new Box(
+            this.xmin - extension,
+            this.ymin - extension,
+            this.xmax + extension,
+            this.ymax + extension
+        )
     }
 
     /**
